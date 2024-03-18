fcdd1d12825a80ae1a66b8e609ac4976468d2bcc
VenujaWickramaarachchi <venuja071@gmail.com>
2024-03-18 10:34:09 +0530

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,26 @@
+const About = () => {
+  return (
+    <>
+      <div className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center'>
+        <h1 className='text-4xl font-bold leading-none tracking-tight sm:text-6xl'>
+          We love
+        </h1>
+        <div className='stats bg-primary shadow'>
+          <div className='stat'>
+            <div className='stat-title text-primary-content text-4xl font-bold tracking-widest'>
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className='mt-6 text-lg leading-8 max-w-2xl mx-auto'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quis
+        fuga libero eligendi mollitia recusandae rem labore impedit alias ullam
+        corrupti, esse eveniet iste quasi aut quae magni quaerat! Totam
+        recusandae perspiciatis ducimus id voluptatum voluptatem repudiandae ab
+        laborum in!
+      </p>
+    </>
+  )
+}
+export default About
---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import cartReducer from './Features/cart/cartSlice'
+import userReducer from './Features/user/userSlice'
+
+export const store = configureStore({
+  reducer: {
+    cartState: cartReducer,
+    userState: userReducer,
+  },
+})
